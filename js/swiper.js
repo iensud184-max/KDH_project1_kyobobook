94ef55a05980cd9c6f495b475bd9cb3c21235c81
iensud184-max <iensud184@gmail.com>
2026-03-24 14:10:10 +0900

    20260324_작업전

--- js/swiper.js
+++ js/swiper.js
@@ -1,18 +1,64 @@
-//  #slider
-var swiper = new Swiper(".mySwiper", {
-            pagination: {
-                el: ".swiper-pagination",
-                type: "fraction",
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
+//  main_swiper
+var mainswiper = new Swiper(".main_swiper", {
+    pagination: {
+        el: "#slider .swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: "#slider .swiper-button-next",
+        prevEl: "#slider .swiper-button-prev",
+    },
+});
 
-// swiper
-var swiper = new Swiper(".mySwiper", {
+// swiper_right
+var mainswiper_right = new Swiper(".swiper_right", {
       pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper_right .swiper-pagination",
+        dynamicBullets: true,
       },
     });
+
+// Recommendation
+var Recommendation_swiper = new Swiper(".Recommendation_Swiper", {
+    slidesPerView: 6,
+    spaceBetween: 25,
+    pagination: {
+        el: "#Recommendation .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#Recommendation .swiper-button-next",
+        prevEl: "#Recommendation .swiper-button-prev",
+    },
+    slidesPerGroup: 6
+});
+
+// Recommendation_2
+var Recommendation_2_swiper = new Swiper(".Recommendation_2_Swiper", {
+    slidesPerView: 6,
+    spaceBetween: 25,
+    pagination: {
+        el: "#Recommendation_2 .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#Recommendation_2 .swiper-button-next",
+        prevEl: "#Recommendation_2 .swiper-button-prev",
+    },
+    slidesPerGroup: 6
+});
+
+// casting_slider
+var casting_slider_Swiper = new Swiper(".casting_slider_Swiper", {
+    slidesPerView: 5,
+    spaceBetween: 25,
+    pagination: {
+        el: "#casting .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#casting .swiper-button-next",
+        prevEl: "#casting .swiper-button-prev",
+    },
+    slidesPerGroup: 5
+});
